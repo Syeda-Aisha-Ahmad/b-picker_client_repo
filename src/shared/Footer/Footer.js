c1488fd6d0d235c09a6f279d77c5f95030f8ba69
SyedaAishaAhmad <18aishaahmad@gmail.com>
2022-11-26 00:48:40 +0600

    Navbar, banner, router, primary layouts, daisyui themes

---
+++ src/shared/Footer/Footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="p-10 mt-20 bg-black">
+            <div className="footer  text-lg">
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </div>
+            <div className='text-center mt-20'>
+                <p>Copyright © 2022 - All right reserved by ACME Industries Ltd</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#4cc0c0",
+
+          "secondary": "#98dede",
+
+          "accent": "#149999",
+
+          "neutral": "#282934",
+
+          "base-100": "#F9F8FB",
+
+          "info": "#3A8FD4",
+
+          "success": "#17976C",
+
+          "warning": "#F08C28",
+
+          "error": "#F46783",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
